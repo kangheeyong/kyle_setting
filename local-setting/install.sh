48ecf6576a8549b6f4fac9db40c6d37e2b517cdf
root <cagojeiger@naver.com>
2020-02-29 08:31:59 +0000

    edit

--- local-setting/install.sh
+++ local-setting/install.sh
@@ -11,6 +11,6 @@ mkdir /root/tmp
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 mkdir -p ~/.vim/autoload ~/.vim/bundle 
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-git clone https://github.com/tpope/vim-sensible.git ~/.vim/bundle
+git clone https://github.com/tpope/vim-sensible.git ~/.vim/bundle/vim-sensible/
 cp -r colors ~/.vim/colors
 cp .vimrc ~/.vimrc
