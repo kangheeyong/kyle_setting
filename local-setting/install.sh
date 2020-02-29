afee217bff7b5c9eff6e5bde6aa6a4ee5a64de56
root <cagojeiger@naver.com>
2020-02-29 05:57:41 +0000

    add setting

--- local-setting/install.sh
+++ local-setting/install.sh
@@ -3,7 +3,7 @@ apt-get install vim -y
 apt-get install curl -y
 apt-get install zsh -y
 apt-get install zsh-completions -y
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"  
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"  --skip-chsh  --unattended 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 cp .zshrc ~/.zshrc
