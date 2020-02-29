c694fecbca40df7724f472c520402f38e434b4bd
kangheeyong <cagojeiger@naver.com>
2020-02-29 14:35:24 +0900

    Update install.sh

--- local-setting/install.sh
+++ local-setting/install.sh
@@ -5,4 +5,5 @@ apt-get install zsh-completions -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" -n 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+cp .zshrc ~/.zshrc
 
