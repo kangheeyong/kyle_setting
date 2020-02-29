3dbf660d72a99dc62f90325b85530db647c7553f
root <cagojeiger@naver.com>
2020-02-29 05:46:02 +0000

    edit

--- local-setting/install.sh
+++ local-setting/install.sh
@@ -1,8 +1,9 @@
+git config --global user.email cagojeiger@naver.com
 apt-get install vim -y
 apt-get install curl -y
 apt-get install zsh -y
 apt-get install zsh-completions -y
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" -n 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"  
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 cp .zshrc ~/.zshrc
