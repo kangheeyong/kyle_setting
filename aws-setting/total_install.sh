0bd3cc9816cfbc6f5dc73732ac4e569825d77b2d
Ubuntu <ubuntu@ip-172-26-13-193.ap-northeast-2.compute.internal>
2020-06-18 08:41:38 +0000

    edit zsh isntall

--- aws-setting/total_install.sh
+++ aws-setting/total_install.sh
@@ -12,7 +12,8 @@ sudo apt-get install ctags -y
 mkdir /home/ubuntu/tmp
 
 sudo apt-get install zsh -y
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"  --unattended 
+wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+sh install.sh --unattended
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 cp .zshrc ~/.zshrc
