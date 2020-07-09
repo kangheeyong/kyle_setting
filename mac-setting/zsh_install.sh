a655c6b7ee6810b9f9e8a8905e45b8d58bc35218
강희용 <kangheeyong@ganghuicbookpro.lan>
2020-07-09 13:28:00 +0900

    add mac setting

---
+++ mac-setting/zsh_install.sh
@@ -0,0 +1,7 @@
+brew install zsh wget
+wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+sh install.sh --unattended
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+cp .zshrc ~/.zshrc
+
