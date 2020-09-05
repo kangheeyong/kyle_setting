1522b808e0db63125b1e2cf19b24bec13b91d32d
heeyong <heeyong@heeyongs-MacBook-Pro.local>
2020-09-05 13:27:50 +0900

    update setting

--- mac-setting/total_install.sh
+++ mac-setting/total_install.sh
@@ -1,12 +1,5 @@
-
-brew install wget
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-mkdir -p ~/.vim/autoload ~/.vim/bundle 
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-git clone https://github.com/tpope/vim-sensible.git ~/.vim/bundle/vim-sensible/
-cp -r colors ~/.vim/colors
-cp .vimrc ~/.vimrc
-brew install ctags
-mkdir ~/temp
-
-
+#!/usr/bin/env bash
+sh brew_install.sh
+sh vim_install.sh
+sh tmux_install.sh
+sh zsh_install.sh
